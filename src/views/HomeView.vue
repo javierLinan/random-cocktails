7d62f92588a6da4877037beeea682ed7d791cb2a
Javier Linan <josejavier.linanmanzaneda.partner@decathlon.com>
2022-06-24 14:20:32 +0200

    feat: Display three random drinks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onBeforeMount, ref, type Ref } from "vue";
+import { RouterLink } from "vue-router";
+
+type Recipe = {
+  idDrink: string;
+  strDrink: string;
+};
+
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1";
+const drinks: Ref<Recipe[]> = ref([]);
+
+onBeforeMount(async () => {
+  drinks.value = await Promise.all([
+    loadRandomDrink(),
+    loadRandomDrink(),
+    loadRandomDrink(),
+  ]);
+});
+
+async function loadRandomDrink(): Promise<Recipe> {
+  const url = `${API_URL}/random.php`;
+  return (await (await fetch(url)).json()).drinks[0];
+}
+</script>
 
 <template>
-  <main></main>
+  <header>
+    <h1>Random drinks</h1>
+    <nav>
+      <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section>
+      <h2>Drinks</h2>
+      <article v-for="drink in drinks" :key="drink.idDrink">
+        <h3>{{ drink.strDrink }}</h3>
+      </article>
+    </section>
+  </main>
 </template>

56cc0aa4667274714e1175fce7368181aa7c90e2
José Javier Liñán Manzaneda <javier.linmanzaneda@gmail.com>
2022-06-24 14:28:59 +0200

    Draft (#2)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import { onBeforeMount, ref, type Ref } from "vue";
+import { RouterLink } from "vue-router";
+
+type Recipe = {
+  idDrink: string;
+  strDrink: string;
+};
+
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1";
+const drinks: Ref<Recipe[]> = ref([]);
+
+onBeforeMount(async () => {
+  drinks.value = await Promise.all([
+    loadRandomDrink(),
+    loadRandomDrink(),
+    loadRandomDrink(),
+  ]);
+});
+
+async function loadRandomDrink(): Promise<Recipe> {
+  const url = `${API_URL}/random.php`;
+  return (await (await fetch(url)).json()).drinks[0];
+}
 </script>
 
 <template>
+  <header>
+    <h1>Random drinks</h1>
+    <nav>
+      <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
   <main>
-    <TheWelcome />
+    <section>
+      <h2>Drinks</h2>
+      <article v-for="drink in drinks" :key="drink.idDrink">
+        <h3>{{ drink.strDrink }}</h3>
+      </article>
+    </section>
   </main>
 </template>

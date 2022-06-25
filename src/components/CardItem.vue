fca4df38f4c78e3f0e53f2cdc0cf725424157786
José Javier Liñán Manzaneda <javier.linmanzaneda@gmail.com>
2022-06-25 17:36:23 +0200

    refactor: Reorganize in components (#6)

---
+++ src/components/CardItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card-item">
+    <div class="card-item__image">
+      <slot name="image"></slot>
+      <div class="card-item__overlapped-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-item__image {
+  position: relative;
+  overflow: hidden;
+  border-radius: 0.6em;
+}
+
+.card-item__image img {
+  transition-duration: 150ms;
+  border-radius: 0.6em;
+}
+.card-item__image img:hover {
+  transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
+  transition-property: transform;
+  transition-duration: 150ms;
+  transform: scale(1.1);
+}
+
+.card-item__overlapped-content {
+  position: absolute;
+  bottom: 0;
+  padding: var(--s0);
+}
+</style>
---
+++ src/components/DrinkCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import StackLayout from "./StackLayout.vue";
+import type { DrinkDto } from "../dtos/DrinkDto";
+import TagItem from "./TagItem.vue";
+import CardItem from "./CardItem.vue";
+
+defineProps<{
+  drink: DrinkDto;
+}>();
+</script>
+
+<template>
+  <article class="drink-card">
+    <StackLayout>
+      <h3 class="text-center one-line-clamp">{{ drink.strDrink }}</h3>
+      <CardItem>
+        <template v-slot:image>
+          <img :src="drink.strDrinkThumb" />
+        </template>
+
+        <StackLayout>
+          <div v-if="!!drink.strIngredient1">
+            <TagItem>{{ drink.strIngredient1 }}</TagItem>
+          </div>
+          <div v-if="!!drink.strIngredient2">
+            <TagItem>{{ drink.strIngredient2 }}</TagItem>
+          </div>
+          <div v-if="!!drink.strIngredient3">
+            <TagItem>{{ drink.strIngredient3 }}</TagItem>
+          </div>
+          <div v-if="!!drink.strIngredient4">
+            <TagItem>...</TagItem>
+          </div>
+        </StackLayout>
+      </CardItem>
+    </StackLayout>
+  </article>
+</template>
+
+<style></style>
